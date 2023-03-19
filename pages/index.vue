b5aeffcb13a5230fd74269dce9381c29081ef3fe
Norsse <raphael.pastre@gmail.com>
2023-03-19 22:43:19 +0100

    improvement: overflow hidden on the x mostly due to main layer animation

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,7 @@
 <template>
-  <MainLayer />
+  <div>
+    <MainLayer />
+  </div>
 </template>
 
 <script>
@@ -13,4 +15,8 @@ export default {
 };
 </script>
 
-<style></style>
+<style lang="scss">
+body {
+  overflow-x: hidden;
+}
+</style>
