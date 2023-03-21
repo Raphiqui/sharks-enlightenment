c95c3136078e3353906a268703c6d7c0dcee7eb5
Norsse <raphael.pastre@gmail.com>
2023-03-21 22:04:18 +0100

    improvement: better transition between backgrounds + insight for user to scroll

--- components/FullImage.vue
+++ components/FullImage.vue
@@ -20,12 +20,23 @@ export default {
 
 <style scoped lang="scss">
 .full-img-container {
-  height: 75vh;
+  height: 90vh;
   width: 100%;
   background-repeat: no-repeat;
   background-size: cover;
   background-position: center;
 
   position: relative;
+
+  &::after {
+    content: "";
+    display: flex;
+    z-index: -1;
+    position: absolute;
+    background: linear-gradient(to bottom, #4c9b8c, #002e66);
+    bottom: -5px;
+    height: 10px;
+    width: 100%;
+  }
 }
 </style>
