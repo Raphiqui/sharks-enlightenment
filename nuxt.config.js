f879d6e10fc00712254f2a24161188e9e970aeb8
raphael <raphael.pastre@gmail.com>
2024-09-17 20:03:56 +0200

    Nuxt 3 config migration

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,73 +1,52 @@
-export default {
-  // Disable server-side rendering: https://go.nuxtjs.dev/ssr-mode
-  ssr: false,
-
-  // Global page headers: https://go.nuxtjs.dev/config-head
-  head: {
-    title: 'Sharks enlightenment',
-    htmlAttrs: {
-      lang: 'en'
-    },
-    bodyAttrs: {
-      class: ['my-style']
-    },
-    meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' },
-      { name: 'format-detection', content: 'telephone=no' }
-    ],
-    link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon-32x32.png' }
-    ]
-  },
-
-  // Global CSS: https://go.nuxtjs.dev/config-css
+export default defineNuxtConfig({
+  // Global CSS: https://v3.nuxtjs.org/docs/directory-structure/nuxt.config#css
   css: [
     // SCSS file in the project
     '~/assets/css/main.scss'
   ],
 
-  // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
+  // Plugins to run before rendering page: https://v3.nuxtjs.org/docs/directory-structure/plugins
   plugins: [
   ],
 
-  // Auto import components: https://go.nuxtjs.dev/config-components
+  // Auto import components: https://v3.nuxtjs.org/docs/directory-structure/components
   components: true,
 
-  // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
+  // Modules for dev and build (recommended): https://v3.nuxtjs.org/docs/directory-structure/modules
   buildModules: [
-    // https://go.nuxtjs.dev/tailwindcss
-    '@nuxtjs/tailwindcss',
-    [
-      'nuxt-i18n',
-      {
-        locales: [
-          { code: 'fr', iso: 'fr-FR', file: 'fr-FR.json', name: 'FR' },
-          { code: 'en', iso: 'en-EN', file: 'en-EN.json', name: 'EN' },
-          { code: 'es', iso: 'es-ES', file: 'es-ES.json', name: 'ES' },
-        ],
-        defaultLocale: 'fr',
-        langDir: 'lang/',
-        lazy: true,
-        strategy: 'no_prefix',
-        seo: true,
-      }
-    ]
   ],
 
-  // Modules: https://go.nuxtjs.dev/config-modules
+  // Modules: https://v3.nuxtjs.org/docs/directory-structure/modules
   modules: [
-    '@nuxtjs/dotenv',
     '@nuxt/image',
     '@nuxtjs/cloudinary',
+    '@nuxtjs/i18n',
   ],
+
+  i18n: {
+    locales: [
+      { code: 'fr', iso: 'fr-FR', file: 'fr-FR.json', name: 'FR' },
+      { code: 'en', iso: 'en-EN', file: 'en-EN.json', name: 'EN' },
+      { code: 'es', iso: 'es-ES', file: 'es-ES.json', name: 'ES' },
+    ],
+    defaultLocale: 'fr',
+    langDir: 'lang/',
+    lazy: true,
+    strategy: 'no_prefix',
+    seo: true,
+  },
 
   cloudinary: {
     cloudName: process.env.CLOUDNAME,
   },
 
-  // Build Configuration: https://go.nuxtjs.dev/config-build
+  // Build Configuration: https://v3.nuxtjs.org/docs/directory-structure/nuxt.config#build
   build: {
+  },
+
+  // Server Configuration: https://v3.nuxtjs.org/docs/directory-structure/nuxt.config#server
+  server: {
+    host: process.env.NUXT_HOST || 'localhost',
+    port: process.env.NUXT_PORT || 3000
   }
-}
+});
