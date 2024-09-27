299fbc627770f7fa397cceb0dd0506ba49be5733
raphaël pastre <raphael.pastre@gmail.com>
2024-09-27 15:31:06 +0200

    Patch transition

--- pages/home/interactive.vue
+++ pages/home/interactive.vue
@@ -7,79 +7,81 @@
 
       <div class="question-container" v-if="questionIndex < questions.length">
         <div class="title-container title">
-          <!-- <transition name="fade"> -->
-          <h2 v-if="showQuestion">
-            {{ $t(`quiz.${questions[questionIndex].id}.text`) }}&nbsp;?
-          </h2>
-          <!-- </transition> -->
+          <transition name="fade">
+            <h2 v-if="showQuestion">
+              {{ $t(`quiz.${questions[questionIndex].id}.text`) }}&nbsp;?
+            </h2>
+          </transition>
         </div>
 
         <div class="option-container">
-          <!-- <transition name="fade"> -->
-          <div v-if="show" class="option-container-content">
-            <div
-              class="option"
-              v-for="(response, index) in questions[questionIndex].responses"
-              @click="selectOption(index)"
-              :key="index"
-            >
-              {{ index | charIndex }}.
-              {{
-                $t(
-                  `quiz.${questions[questionIndex].id}.responses.${response.id}`
-                )
-              }}
-            </div>
-          </div>
-          <div v-if="showCorrectAnswer" class="correct-answer">
-            <div class="correct-answer-container">
-              <div class="correct-response-label-container">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="24"
-                  height="24"
-                  viewBox="0 0 24 24"
-                  fill="none"
-                  stroke="green"
-                  stroke-width="2"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  style="margin: 0 auto 20px auto; width: 30px; height: 30px"
-                  v-if="isCorrect"
-                >
-                  <path d="M22 11.08V12a10 10 0 1 1-5.93-9.14"></path>
-                  <polyline points="22 4 12 14.01 9 11.01"></polyline>
-                </svg>
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="24"
-                  height="24"
-                  viewBox="0 0 24 24"
-                  fill="none"
-                  stroke="red"
-                  stroke-width="2"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  style="margin: 0 auto 20px auto; width: 30px; height: 30px"
-                  v-else
-                >
-                  <circle cx="12" cy="12" r="10"></circle>
-                  <line x1="15" y1="9" x2="9" y2="15"></line>
-                  <line x1="9" y1="9" x2="15" y2="15"></line></svg
-                >{{ $t("correct") }}&nbsp;:
+          <transition name="fade">
+            <div v-if="show" class="option-container-content">
+              <div
+                class="option"
+                v-for="(response, index) in questions[questionIndex].responses"
+                @click="selectOption(index)"
+                :key="index"
+              >
+                {{ index | charIndex }}.
                 {{
                   $t(
-                    `quiz.${questions[questionIndex].id}.responses.${correctResponse}`
+                    `quiz.${questions[questionIndex].id}.responses.${response.id}`
                   )
                 }}
               </div>
-              {{ $t(`quiz.${questions[questionIndex].id}.answer`) }}
             </div>
-            <div class="next-button button" @click="next()">
-              {{ $t("next") }}
+          </transition>
+          <transition name="fade">
+            <div v-if="showCorrectAnswer" class="correct-answer">
+              <div class="correct-answer-container">
+                <div class="correct-response-label-container">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="24"
+                    height="24"
+                    viewBox="0 0 24 24"
+                    fill="none"
+                    stroke="green"
+                    stroke-width="2"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    style="margin: 0 auto 20px auto; width: 30px; height: 30px"
+                    v-if="isCorrect"
+                  >
+                    <path d="M22 11.08V12a10 10 0 1 1-5.93-9.14"></path>
+                    <polyline points="22 4 12 14.01 9 11.01"></polyline>
+                  </svg>
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="24"
+                    height="24"
+                    viewBox="0 0 24 24"
+                    fill="none"
+                    stroke="red"
+                    stroke-width="2"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    style="margin: 0 auto 20px auto; width: 30px; height: 30px"
+                    v-else
+                  >
+                    <circle cx="12" cy="12" r="10"></circle>
+                    <line x1="15" y1="9" x2="9" y2="15"></line>
+                    <line x1="9" y1="9" x2="15" y2="15"></line></svg
+                  >{{ $t("correct") }}&nbsp;:
+                  {{
+                    $t(
+                      `quiz.${questions[questionIndex].id}.responses.${correctResponse}`
+                    )
+                  }}
+                </div>
+                {{ $t(`quiz.${questions[questionIndex].id}.answer`) }}
+              </div>
+              <div class="next-button button" @click="next()">
+                {{ $t("next") }}
+              </div>
             </div>
-          </div>
-          <!-- </transition> -->
+          </transition>
         </div>
       </div>
 
