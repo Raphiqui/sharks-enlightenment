06ed8059d4922978723c2ca1cc1df6364cb5ffae
norsse <raphael.pastre@gmail.com>
2025-04-19 20:42:08 +0200

    Fix desprecation sass warning

--- layouts/default.vue
+++ layouts/default.vue
@@ -210,11 +210,11 @@ header {
 
       &:hover::after {
 
-        @media (max-width: 768px) {
-          width: 0px
-        }
-
         width: 100%;
+
+        @media (max-width: 768px) {
+          width: 0px;
+        }
       }
     }
 
