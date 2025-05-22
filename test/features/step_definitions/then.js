0e8518429bde384a505440bb73b576507695be90
norsse <raphael.pastre@gmail.com>
2025-05-22 18:39:58 +0200

    Add test case scenario for navigation

--- test/features/step_definitions/then.js
+++ test/features/step_definitions/then.js
@@ -24,3 +24,37 @@ Then("I should see {string} cards", async function (expectedCount) {
     .count();
   expect(cardCount).toEqual(parseInt(expectedCount, 10));
 });
+
+Then(
+  "I should be able to navigate to the {string} page",
+  async function (navigationExpected) {
+    const currentUrl = await this.page.url();
+
+    const arrowMapping = {
+      next: "rightArrow",
+      previous: "leftArrow",
+    };
+
+    const arrow = this.page.locator(
+      `[data-test-id="${arrowMapping[navigationExpected]}"]`
+    );
+
+    await arrow.click();
+
+    let newUrl = currentUrl;
+    const maxRetries = 20;
+    const delayMs = 250;
+
+    for (let i = 0; i < maxRetries; i++) {
+      newUrl = await this.page.url();
+      if (newUrl !== currentUrl) break;
+      await this.page.waitForTimeout(delayMs);
+    }
+
+    if (newUrl === currentUrl) {
+      throw new Error(
+        `URL did not change after clicking the ${navigationExpected} arrow`
+      );
+    }
+  }
+);
